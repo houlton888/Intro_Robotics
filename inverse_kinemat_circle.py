2701945b125f5f1e449a2569988b0d8e0e575740
houlton888 <147352157+houlton888@users.noreply.github.com>
2023-10-11 09:33:39 -0400

    Update inverse_kinemat_circle.py

--- inverse_kinemat_circle.py
+++ inverse_kinemat_circle.py
@@ -2,9 +2,16 @@ import matplotlib.pyplot as plt
 import math
 import numpy as np
 import time
+import paho.mqtt.client as mqtt
+
+broker_adress = "67.253.32.232"
+client = mqtt.Client("Eli_mqtt")
+client.connect(broker_adress)
+print('Connected to %s MQTT broker' % broker_adress)
+client.subscribe("ME035")
 
 # create circle points
-num_points = 30
+num_points = 20
 L_arm = 1
 # create x_vals
 top_circle_x = np.arange(L_arm/2,(3/2)*L_arm,L_arm/num_points)
@@ -22,21 +29,23 @@ for i in range(num_points*2):
 x = np.append(circle_x_vals,L_arm/2)
 y = np.append(circle_y_vals,0)
 
-print(circle_x_vals)
-print(circle_y_vals)
-
 # Calculate theta for correcponding x,y points
 points = (num_points*2)+1
 theta1 = np.zeros(points)
 theta2 = np.zeros(points)
 
-
 for i in range(points):
     theta2[i] = -math.acos(((x[i]**2+y[i]**2)-(2*L_arm**2))/(2*L_arm**2))
     theta1[i] = -1*(math.atan2(y[i],x[i]) + math.atan2((L_arm*math.sin(theta2[i])),(L_arm+(L_arm*math.cos(theta2[i])))))
 
-print(theta1*180/3.1415)
-print(theta2*180/3.1415)
+for i in range(len(theta1)):
+    message = str("(" + str(np.rad2deg(theta1[i]))+', '+str(np.rad2deg(theta2[i]))+")")
+    client.publish("ME035", message) #Note this is the channel Name
+    time.sleep(.1)
+
+client.publish("theta/tell", message)
+#Done - below is just checking work
+
 x_from_theta = np.zeros(len(x))
 y_from_theta = np.zeros(len(y))
 for i in range(len(x)):
@@ -49,5 +58,5 @@ ax.plot(x_from_theta,y_from_theta)
 ax.set(xlabel='X position', ylabel='Y position', title='Arm position')
 ax.grid()
 plt.show(block=False)
-plt.pause(10)
+plt.pause(2)
 plt.close()
